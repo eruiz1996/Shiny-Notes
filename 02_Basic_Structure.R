58c6c6a7535d77401d4a2526620eec95250d7a4b
Edgar Ruiz <edgarruiztovar@gmail.com>
2024-04-11 19:07:24 -0600

    Adding more inputs and outputs

--- 02_Basic_Structure.R
+++ 02_Basic_Structure.R
@@ -2,21 +2,41 @@ library(shiny)
 library(ggplot2)
 
 ui <- fluidPage(
-  titlePanel("Baby Name Explorer"),
+  titlePanel("Mi edad en 10 años..."),
   sidebarLayout(
-    # to the left
+    # Panel izquierdo
+    #---------------------------------------------------------------------------
     sidebarPanel(
-      textInput("name", 'Enter Name', "David")
+      # input 1: de texto
+      textInput(inputId = 'name', label = "Introduce tu nombre", value = "Edgar"),
+      # input 2: numérico
+      numericInput('edad', "Introduce tu edad", value = 27)
     ),
+    #---------------------------------------------------------------------------
+    
+    # Panel principal
+    #---------------------------------------------------------------------------
     mainPanel(
-      plotOutput('trend')
+      h1("Datos"),
+      h3("Nombre:"),
+      verbatimTextOutput('nombre'),
+      h3("En 10 años tendrá:"),
+      verbatimTextOutput('edad')
     )
+    #---------------------------------------------------------------------------
   )
 )
 
 server <- function(input, output, sesion){
-  output$trend <- renderPlot({
-    ggplot()
+  # output de nombre
+  output$nombre <- renderText({
+    input$name
+  })
+  
+  # output de edad
+  output$edad <- renderText({
+    nueva_edad <- input$edad + 10
+    nueva_edad
   })
 }
 
